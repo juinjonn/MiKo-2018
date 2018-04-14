5b1d0c48d1e89ea4b53e2a7c8366aa5c0b2b800a
junzhong.chen <junzhong.chen@qunar.com>
2018-04-14 12:56:23 +0800

    springboot初始化

---
+++ src/main/java/com/HUST/JuinJonn/MiKo2018/MiKo2018Application.java
@@ -0,0 +1,12 @@
+package com.HUST.JuinJonn.MiKo2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiKo2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiKo2018Application.class, args);
+	}
+}
---
+++ src/test/java/com/HUST/JuinJonn/MiKo2018/MiKo2018ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.HUST.JuinJonn.MiKo2018;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiKo2018ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
