f860b8ef83ddc72b7a74be36cd339f3cc30a89f9
junzhong.chen <junzhong.chen@qunar.com>
2018-05-13 23:35:05 +0800

    基本结构

--- src/main/java/com/HUST/JuinJonn/MiKo2018/MiKo2018Application.java
+++ src/main/java/com/HUST/JuinJonn/MiKo2018/MiKo2018Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiKo2018Application {
+public class  MiKo2018Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiKo2018Application.class, args);
---
+++ src/main/java/com/HUST/JuinJonn/MiKo2018/controller/MikoController.java
@@ -0,0 +1,13 @@
+package com.HUST.JuinJonn.MiKo2018.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MikoController {
+    @RequestMapping(value = "/")
+    public String miko(){
+
+        return "miko";
+    }
+}
